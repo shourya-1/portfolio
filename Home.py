7adf7793a102ac1a0db1c0c95e63e7b251067b14
shourya <bhartam43@gmail.com>
2024-05-31 09:08:23 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,7 +16,9 @@ with col2:
     st.info(content)
 
 content2 = """
-Below you can find some of the apps I have built in Python. Feel free to contact me!
+Below you can find some of the apps I have built in Python. 
+Feel free to contact me!
+
 """
 st.write(content2)
 
